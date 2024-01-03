8bb302902bef8f8a923f4af2f83d9b050cbe323e
Maria Bekish <masha.bekish@gmail.com>
2024-01-03 19:28:36 +0300

    Add user service

--- Domain/Repositories/IUserRepository.cs
+++ Domain/Repositories/IUserRepository.cs
@@ -1,13 +1,8 @@
 ﻿using Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Repositories
 {
-    internal interface IUserRepository
+    public interface IUserRepository
     {
         public User? FindUserByEmail(string email);
         public User CreateUser(User newUser);
---
+++ WebApp/Models/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+
+namespace WebApp.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Payload Payload { get; set; }
+
+        public UserDto(User user, string token)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+            Payload = new Payload(token);
+        }
+    }
+}
