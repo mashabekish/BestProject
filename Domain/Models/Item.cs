f3da3986910005776a9dae423d1b1a6720986fad
Polina <polina.krasnianska@praguecollege.cz>
2024-01-03 16:51:10 +0100

    Models and dtb configuration

---
+++ Domain/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Item
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public Flags Flag { get; set; }
+        public bool IsResolved { get; set; } = false;
+    }
+}
---
+++ Domain/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public User CreateUser(User newUser)
+        {
+            _db.Users.Add(newUser);
+            _db.SaveChanges();
+            return newUser;
+        }
+
+        public User? FindUserByEmail(string email)
+        {
+            return _db.Users.FirstOrDefault(u => u.Email.Equals(email));
+        }
+    }
+}
