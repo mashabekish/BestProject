1fcfd671542b724734e86f01a918cfe3f19bb2c4
Polina <polina.krasnianska@praguecollege.cz>
2024-01-03 20:59:51 +0100

    Make PhoneNumber and Items nullable

--- Domain/Models/Item.cs
+++ Domain/Models/Item.cs
@@ -11,6 +11,8 @@ namespace Domain.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public int UserId { get; set; }
