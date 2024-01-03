527f3a9eaae3fcae06c8f30df9a7c41b09d0dfb1
Polina <polina.krasnianska@praguecollege.cz>
2024-01-03 21:00:11 +0100

    Add data seeding

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Domain.DataSeeder;
 using Newtonsoft.Json;
 using WebApp.Extensions;
 
@@ -21,11 +22,16 @@ public class Program
 
         builder.Services.AddSqlServerDatabase<AppDbContext>();
 
+        builder.Services
+            .AddScoped<IDbSeeder, DbSeeder>();
+
         builder.Services.AddSwagger();
 
         var app = builder.Build();
 
         app.UseAuthentication();
+
+        SeedDatabase();
 
         if (app.Environment.IsDevelopment())
         {
@@ -38,5 +44,14 @@ public class Program
         app.MapControllers();
 
         app.Run();
+
+        async void SeedDatabase()
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbSeeder = scope.ServiceProvider.GetRequiredService<IDbSeeder>();
+               await dbSeeder.SeedData();
+            }
+        }
     }
 }
