146d3fc1b7242a2a0d8e4548460678dd8388c48e
Bohdan Nazarenko <bohimus@gmail.com>
2024-01-04 13:48:10 +0100

    Get items by user id

--- Domain/Abstractions/IItemRepository.cs
+++ Domain/Abstractions/IItemRepository.cs
@@ -6,9 +6,10 @@ public interface IItemRepository
 {
     Task<IEnumerable<Item>> GetFoundItemsAsync();
     Task<IEnumerable<Item>> GetFoundItemsAsync(string category);
+    Task<IEnumerable<Item>> GetFoundItemsAsync(int userId);
     Task<IEnumerable<Item>> GetLostItemsAsync();
     Task<IEnumerable<Item>> GetLostItemsAsync(string category);
-    Task<Item> CreateFoundItemAsync(Item newFoundItem);
-    Task<Item> CreateLostItemAsync(Item newLostItem);
+    Task<IEnumerable<Item>> GetLostItemsAsync(int userId);
+    Task<Item> CreateItemAsync(Item newItem);
     Task<IEnumerable<Item>> GetResolvedItemsAsync();
 }
